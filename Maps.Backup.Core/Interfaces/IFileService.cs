e53264f170cdfbe37a5ceff3547d69d15ad66a3a
ZhengQiwen <zhengqiwen@emsystems-sh.com>
2026-01-29 10:00:25 +0800

    Add project files.

---
+++ Maps.Backup.Core/Interfaces/IFileService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maps.Backup.Core.Interfaces
+{
+    public interface IFileService
+    {
+        string DownLoad(string remotePath,string savePath);
+
+        string UpLoad(string localPath,string targetPath);
+
+        string UnZip(string path);
+
+        List<string> FindFileByExtension(string rootPath, string fileEx, bool recursive);
+    }
+}
---
+++ Maps.Backup.Core/Interfaces/IShellClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maps.Backup.Core.Interfaces
+{
+    public interface IShellClient
+    {
+        void Excete(string command);
+
+        void Excete(string command, Action afterExceted);
+
+    }
+}
