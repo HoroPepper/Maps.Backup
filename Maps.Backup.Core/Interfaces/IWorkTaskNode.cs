8d01fd4234c30fa95ce1fa91b212770a245bf42b
ZhengQiwen <zhengqiwen@emsystems-sh.com>
2026-01-29 12:10:56 +0800

    taskNode

---
+++ Maps.Backup.Core/Interfaces/IWorkTaskNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maps.Backup.Core.Interfaces
+{
+    public interface IWorkTaskNode
+    {
+        string TaskId { get; set; } 
+
+        string TaskName { get; set; }
+
+        string TaskType { get; set; }
+
+        string Execute(object param);
+    }
+}
