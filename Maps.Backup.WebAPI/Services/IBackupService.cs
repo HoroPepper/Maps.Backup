5a71ad419c3c3d1d5aa413f728a5433893a9f0ea
ZhengQiwen <zhengqiwen@emsystems-sh.com>
2026-03-05 17:04:06 +0800

    1

---
+++ Maps.Backup.WebAPI/Services/IBackupService.cs
@@ -0,0 +1,12 @@
+﻿using Maps.Backup.WebAPI.Dtos.Req;
+using Maps.Backup.WebAPI.Dtos.Res;
+
+namespace Maps.Backup.WebAPI.Services
+{
+    public interface IBackupService
+    {
+        Task<RestoreRes> Restore(RestoreReq restoreReq);
+
+        IAsyncEnumerable<RestoreProgressRes> RestoreWithStream(RestoreReq restoreReq, CancellationToken cancellationToken);
+    }
+}
